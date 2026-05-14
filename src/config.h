16e15220d67a532075eafc961531bbbd0657a201
andy-linux <andreas.loehr97@gmail.com>
2026-05-14 22:33:17 +0200

    refactor, new instructions.

--- src/config.h
+++ src/config.h
@@ -1,11 +1,17 @@
-constexpr char *GEMINI_URL =
+#ifndef SRC_CONFIG_H_
+#define SRC_CONFIG_H_
+
+constexpr const char* GEMINI_URL =
     "https://generativelanguage.googleapis.com/v1beta/models/"
     "gemini-3-flash-preview:generateContent";
 
-constexpr char *INSTRUCTIONS =
+constexpr const char* INSTRUCTIONS =
     "You are a helpful assistant that generates Anki flashcards based on the "
     "provided image. "
     "Extract the English word/concept and its Russian translation from "
     "this Duolingo exercise screenshot. Provide a short, concise card "
     "name. If one word in the screenshot is unknown, it will be inside an "
-    "extra speech bubble.";
+    "extra speech bubble, pointing at the word. The bubble contains the word and the translation."
+    "In this case, only translate the word, not the entire sentence.";
+
+#endif  // SRC_CONFIG_H_
