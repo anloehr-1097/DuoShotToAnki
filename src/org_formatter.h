ca48e8438e653d542e9e07978a4d1e9bf0da1441
Andreas Loehr <andreas.loehr97@gmail.com>
2026-05-31 11:05:18 +0200

    format image link

--- src/org_formatter.h
+++ src/org_formatter.h
@@ -1,25 +1,25 @@
 #ifndef ORG_FORMATTER_H
 #define ORG_FORMATTER_H
 
-#include "duo_anki_interface.h"
 #include <filesystem>
 #include <string>
 
+#include "duo_anki_interface.h"
+
 class OrgFormatter {
 private:
-  std::filesystem::path org_file_path;
-  std::filesystem::path image_path; // directory to put images
-  std::string format_header(const std::string &header, int level = 1) const;
-  std::string format_header(const std::string &&header, int level = 1) const;
-  std::string add_properties() const;
+    std::filesystem::path org_file_path;
+    std::filesystem::path image_path;  // directory to put images
+    std::string format_header(const std::string& header, int level = 1) const;
+    std::string format_header(const std::string&& header, int level = 1) const;
+    std::string format_image_link(const std::string& image_link) const;
+    std::string add_properties() const;
 
 public:
-  OrgFormatter(const std::filesystem::path org_file_path,
-               const std::filesystem::path image_path);
-  void append_card(const DuoAnkiResponse &card_info,
-                   const std::string &image_path) const;
+    OrgFormatter(const std::filesystem::path org_file_path, const std::filesystem::path image_path);
+    void append_card(const DuoAnkiResponse& card_info, const std::string& image_path) const;
 };
 
 std::string get_current_date();
 std::string get_current_date_with_day();
-#endif // ORG_FORMATTER_H
+#endif  // ORG_FORMATTER_H
